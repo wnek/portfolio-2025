10da05d40af2d19bcd23546a9d5ba6e1d3abab51
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-10-10 10:35:31 +0200

    Add framer motion and test transitions

---
+++ app/root.tsx
@@ -0,0 +1,63 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useLocation, useOutlet } from "react-router-dom";
+
+import "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  const location = useLocation();
+  const outlet = useOutlet();
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, x: "10%" }}
+        animate={{ opacity: 1, x: "0%" }}
+        exit={{ opacity: 0, x: "-40%" }}
+        transition={{ duration: 0.3 }}
+      >
+        {outlet}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
---
+++ app/routes/lab.tsx
@@ -0,0 +1,5 @@
+export default function Lab() {
+    return (
+        <div>lab</div>
+    )
+}
