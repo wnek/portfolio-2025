812a90c98d3dd7276f10891ccc88d7fc0c02282a
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-10-23 22:03:37 +0200

    Add framer motion: useAnimate, stagger animation for li

--- app/root.tsx
+++ app/root.tsx
@@ -72,7 +72,7 @@ export default function App() {
 
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+
 
 
       </body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,43 @@
+import { motion, stagger, useAnimate } from "framer-motion";
+import { useEffect } from "react";
+
 export default function Index() {
+
+  const [scope, animate] = useAnimate();
+
+
+
+  useEffect(() => {
+    animate("li", { y: -20 }, { delay: stagger(0.1) })
+  }, [])
+
   return (
     <>
       <div className="bg-white p-8">
         <section className="m-auto max-w-screen-md flex flex-col gap-8">
           <h1 className="~text-[2rem]/[5rem]">Growth by design</h1>
-          <p>Driving products growth by understanding the why behind users, business, and technology</p>
+
         </section>
 
         <section className="m-auto max-w-screen-md flex flex-col gap-8">
           <p className="text-sm text-orange-500">Manifesto</p>
-          <h2 className="text-4xl">According to Harvard Business Review, 75% of startups don't succeed. I know the frustration of working hard and not seeing results. I also know the solution.</h2>
+
           <p>Every new concept appears promising. It's easy to simply follow in the footsteps of competitors, trust your instincts, or optimistically hope for the best. However, our judgment is often clouded by subtle biases, leading to less than rational decision-making.
           </p>
 
           <p>Impactful design goes beyond just aesthetic appeal. It must directly contribute to business performance. My approach incorporates design, psychology, and technology, with a specific focus on driving product growth.</p>
 
-          <ul>
+          <ul ref={scope} className="flex flex-col gap-1">
             <li className="flex gap-4 bg-stone-100 p-4 rounded-md">
               Focus on the most critical performance metrics.
+            </li>
+
+            <li className="flex gap-4 bg-stone-100 p-4 rounded-md">
+              Identify and prioritize customer needs and expectations.
+            </li>
+
+            <li className="flex gap-4 bg-stone-100 p-4 rounded-md">
+              Create user experiences that drive conversions and growth.
             </li>
           </ul>
         </section>
