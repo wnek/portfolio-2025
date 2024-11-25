f5e4c19ce82d25a5733ba858977178e0fed57722
Mariusz Wnęk <wnek.mariusz@gmail.com>
2024-11-25 21:59:26 +0100

    Content tweaks

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,58 +1,32 @@
-import { stagger, useAnimate, useInView } from "framer-motion";
-import { motion } from "framer-motion-3d"
 import { useEffect } from "react";
-
-import { Canvas } from "@react-three/fiber";
-import { CameraControls, SoftShadows } from "@react-three/drei";
 
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 
 export default function Index() {
 
   const lenis = useLenis(({ scroll }) => {
-    // called every scroll
+
   })
 
-  const [scope, animate] = useAnimate();
-  const inView = useInView(scope);
-
-  useEffect(() => {
-
-    if (inView) {
-      animate("li", { y: -5 }, { delay: stagger(0.1) })
-    }
-
-  }, [inView])
 
   return (
     <ReactLenis root>
       <div className="p-8">
-        <div className="w-full h-screen border-red-500 border-2">
-          <Canvas>
-            <CameraControls />
-            <SoftShadows />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[10, 10, 10]} intensity={1} />
-            <motion.mesh>
-              <boxGeometry />
-              <meshStandardMaterial />
-            </motion.mesh>
-          </Canvas>
-        </div>
+
         <section className="m-auto max-w-screen-md flex flex-col gap-8">
           <h1 className="~text-[2rem]/[5rem]">Growth by design</h1>
 
         </section>
 
         <section className="m-auto max-w-screen-md flex flex-col gap-8">
-          <p className="text-sm text-orange-500">Manifesto</p>
+
 
           <p>Every new concept appears promising. It's easy to simply follow in the footsteps of competitors, trust your instincts, or optimistically hope for the best. However, our judgment is often clouded by subtle biases, leading to less than rational decision-making.
           </p>
 
           <p>Impactful design goes beyond just aesthetic appeal. It must directly contribute to business performance. My approach incorporates design, psychology, and technology, with a specific focus on driving product growth.</p>
 
-          <ul ref={scope} className="flex flex-col gap-1">
+          <ul className="flex flex-col gap-1">
             <li className="flex gap-4 bg-white p-4 rounded-md">
               Focus on the most critical performance metrics.
             </li>
